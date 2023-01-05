9ce0b2403a4ff405b83fd5576afe1b6788ff8b68
kaairasif <asifcse22@gmail.com>
2023-01-05 14:27:25 +0600

    Update Home.jsx

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,10 +3,18 @@ import cartSlice from '../data/cartSlice'
 import { fetchAllProducts } from '../data/productSlice'
 import '../styles/home.scss'
 import { useEffect } from 'react'
+import { IoAddCircleOutline, IoMedkitSharp } from "react-icons/io5";
+import Counter from './Counter'
+
+import { Button, ProductCard } from './styled-components'
+
+
+
 const Home = () => {
   const state = useSelector((state) => state)  
 
   console.log(state)
+
   const { cart, products } = state
   const { addToCart, removeFromCart } = cartSlice.actions
   const dispatch = useDispatch()
@@ -15,32 +23,48 @@ const Home = () => {
     dispatch(fetchAllProducts('http://localhost:3000/products'))
   }, [dispatch]);
 
+
   return (
     <div className="container product-catalogue">
-     <h2>Product hub page</h2>
+
+     <Counter />
+
+     <h2><IoMedkitSharp style={{fontSize: "30px"}} /> Product hub page</h2>
+
+     <Button>Styled Components</Button> 
       <div className="row">
         {products.data?.map((product) => {
           return (
             <div className="wrapper col-md-4" key={product.id}>
-              <div className="card">
+              <ProductCard isAdded={!cart.cartProductIds.includes(product.id)}>
                 <img className="card-img-top center-block" src={product.imageUrl} alt="Card cap" />
 
                 <div className="card-body text-center">
+                
+                <span>{!cart.cartProductIds.includes(product.id) ? "A" : "B"}</span>
+
                   <h5 className="card-title">{product.name}</h5>
                   <p className="card-text">${product.price}</p>
 
+                  
+
                   {!cart.cartProductIds.includes(product.id) && (
-                    <button className="btn btn-primary" onClick={() => dispatch(addToCart(product.id))}>
-                      Add to cart
+                    <button style={{ display : "inline-flex", fontSize: "20px"}} className="btn btn-primary" onClick={() => {
+                     dispatch(addToCart(product.id, product.isSelected))
+                    }
+                    }>
+                      <IoAddCircleOutline style={{ fontSize: "25px", color: "chartreuse", marginRight: "5px" }} /> Add to cart
                     </button>
                   )}
                   {cart.cartProductIds.includes(product.id) && (
-                    <button className="btn btn-primary" onClick={() => dispatch(removeFromCart(product.id))}>
+                    <button style={{ display : "inline-flex", fontSize: "20px"}} className="btn btn-primary" onClick={() => dispatch(removeFromCart(product.id))}>
                       Remove from cart
                     </button>
                   )}
+
+                  
                 </div>
-              </div>
+              </ProductCard>
             </div>
           )
         })}
